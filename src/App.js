5274dff1289a7fc99556460ba62e99ecdedff389
Chris Mems <christopher.mems@.com>
2023-02-02 10:52:25 -0600

    create Starship component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import StartshipList from './StarshipList/StartshipList';
 
 function App() {
   return (
     <div>
-      
+      <StartshipList />
     </div>
   );
 }
---
+++ src/StarshipList/StartshipList.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react"
+import { getAllStarships } from "../services/sw-api"
+
+function StartshipList() {
+  return (
+    <div>StartshipList</div>
+  )
+}
+
+export default StartshipList
